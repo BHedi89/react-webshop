21df5c2e965880502e7572a65d4700cf13a30827
BHedi89 <hedi.besenyei89@gmail.com>
2022-02-02 15:07:04 +0100

    mobile first navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -13,14 +13,14 @@ const Navbar = () => {
             <header>
                 <nav>
                     <ul>
-                        <li className={isActive ? classes.visible : null}><a href="#">Face</a></li>
-                        <li className={isActive ? classes.visible : null}><a href="#">Lips</a></li>
-                        <li className={isActive ? classes.visible : null}><a href="#">Eyes</a></li>
-                        <li className={`${classes.logo} ${classes.logotrigger}`} 
+                        <li className={`${isActive ? classes.visible : null} ${classes.one}`}><a href="#">Face</a></li>
+                        <li className={`${isActive ? classes.visible : null} ${classes.two}`}><a href="#">Lips</a></li>
+                        <li className={`${isActive ? classes.visible : null} ${classes.three}`}><a href="#">Eyes</a></li>
+                        <li className={`${classes.logo} ${classes.four}`} 
                             onClick={logoTrigger}><a href="#">WM<br/><span>Wonderful Makeups</span></a></li>
-                        <li className={isActive ? classes.visible : null}><a href="#">Nails</a></li>
-                        <li className={isActive ? classes.visible : null}><a href="#">Contact</a></li>
-                        <li className={isActive ? classes.visible : null}><a href="#">Login</a></li>
+                        <li className={`${isActive ? classes.visible : null} ${classes.five}`}><a href="#">Nails</a></li>
+                        <li className={`${isActive ? classes.visible : null} ${classes.six}`}><a href="#">Contact</a></li>
+                        <li className={`${isActive ? classes.visible : null} ${classes.seven}`}><a href="#">Login</a></li>
                     </ul>
                 </nav>
             </header>
