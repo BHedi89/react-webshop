402446fb7c6b842103d76b89c391c9fbfa0b92be
BHedi89 <hedi.besenyei89@gmail.com>
2022-02-04 17:02:24 +0100

    add new image slide to main page and responsive breakpoints help

--- src/components/layout/ImageCarousel.js
+++ src/components/layout/ImageCarousel.js
@@ -4,17 +4,31 @@ const ImageCarousel = () =>  {
     return (
         <>
             <div className={classes.container}>
-                <div className={classes.carousel}>
-                    <div className={classes.carousel__face}></div>
-                    <div className={classes.carousel__face}></div>
-                    <div className={classes.carousel__face}></div>
-                    <div className={classes.carousel__face}></div>
-                    <div className={classes.carousel__face}></div>
-                    <div className={classes.carousel__face}></div>
-                    <div className={classes.carousel__face}></div>
-                    <div className={classes.carousel__face}></div>
-                    <div className={classes.carousel__face}></div>
-                </div>
+                <ul className={classes.slides}>
+                    <li id="slide1"><img src={require("../images/carousel/slide1.jpg")} alt="" /></li>
+                    <li id="slide2"><img src={require("../images/carousel/slide2.jpg")} alt="" /></li>
+                    <li id="slide3"><img src={require("../images/carousel/slide3.jpg")} alt="" /></li>
+                    <li id="slide4"><img src={require("../images/carousel/slide4.jpg")} alt="" /></li>
+                    <li id="slide5"><img src={require("../images/carousel/slide5.jpg")} alt="" /></li>
+                </ul>
+
+                <ul className={classes.thumbnails}>
+                    <li>
+                        <a href="#slide1"><img src={require("../images/carousel/slide1.jpg")} /></a>
+                    </li>
+                    <li>
+                        <a href="#slide2"><img src={require("../images/carousel/slide2.jpg")} /></a>
+                    </li>
+                    <li>
+                        <a href="#slide3"><img src={require("../images/carousel/slide3.jpg")} /></a>
+                    </li>
+                    <li>
+                        <a href="#slide4"><img src={require("../images/carousel/slide4.jpg")} /></a>
+                    </li>
+                    <li>
+                        <a href="#slide5"><img src={require("../images/carousel/slide5.jpg")} /></a>
+                    </li>
+                </ul>
             </div>
         </>
     );
