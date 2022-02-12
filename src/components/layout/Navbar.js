145ff60dd873c0d133efba1506425707952a56ee
BHedi89 <hedi.besenyei89@gmail.com>
2022-02-12 18:42:51 +0100

    aad cart icon to the navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,31 +1,71 @@
 import classes from "./Navbar.module.css";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-    const [isActive, setActive] = useState(false);
+  const [isActive, setActive] = useState(false);
 
-    const logoTrigger = () => {
-        setActive(!isActive);
-    }
+  const logoTrigger = () => {
+    setActive(!isActive);
+  };
 
-    return (
-        <>
-            <header>
-                <nav>
-                    <ul>
-                        <li className={`${isActive ? classes.visible : null} ${classes.one}`}><a href="#">Face</a></li>
-                        <li className={`${isActive ? classes.visible : null} ${classes.two}`}><a href="#">Lips</a></li>
-                        <li className={`${isActive ? classes.visible : null} ${classes.three}`}><a href="#">Eyes</a></li>
-                        <li className={`${classes.logo} ${classes.four}`} 
-                            onClick={logoTrigger}><a href="#">WM<br/><span>Wonderful Makeups</span></a></li>
-                        <li className={`${isActive ? classes.visible : null} ${classes.five}`}><a href="#">Nails</a></li>
-                        <li className={`${isActive ? classes.visible : null} ${classes.six}`}><a href="#">Contact</a></li>
-                        <li className={`${isActive ? classes.visible : null} ${classes.seven}`}><a href="#">Login</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </>
-    )
-}
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li
+              className={`${isActive ? classes.visible : null} ${classes.one}`}
+            >
+              <a href="#">Face</a>
+            </li>
+            <li
+              className={`${isActive ? classes.visible : null} ${classes.two}`}
+            >
+              <a href="#">Lips</a>
+            </li>
+            <li
+              className={`${isActive ? classes.visible : null} ${
+                classes.three
+              }`}
+            >
+              <a href="#">Eyes</a>
+            </li>
+            <li
+              className={`${classes.logo} ${classes.four}`}
+              onClick={logoTrigger}
+            >
+              <a href="#">
+                WM
+                <br />
+                <span>Wonderful Makeups</span>
+              </a>
+            </li>
+            <li
+              className={`${isActive ? classes.visible : null} ${classes.five}`}
+            >
+              <a href="#">Nails</a>
+            </li>
+            <li
+              className={`${isActive ? classes.visible : null} ${classes.six}`}
+            >
+              <a href="#">Login</a>
+            </li>
+            <li
+              className={`${isActive ? classes.visible : null} ${
+                classes.seven
+              }`}
+            >
+              <a href="#">
+                <FontAwesomeIcon icon={faShoppingBasket} className={classes.icon} />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default Navbar;
