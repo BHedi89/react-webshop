f9acf037a9017b6577bc701c7cf377d747935cd4
BHedi89 <hedi.besenyei89@gmail.com>
2022-02-19 19:47:35 +0100

    add more alert messages

--- src/components/layout/Dropdownbutton.js
+++ src/components/layout/Dropdownbutton.js
@@ -3,23 +3,38 @@ import classes from "./Dropdownbutton.module.css";
 import { Link } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
 import { UserDataContext } from "../login/UserDataContext";
+import Alert from "./Alert";
 
 const Dropdownbutton = () => {
     const [open, setOpen] = React.useState(false);
     let userContext = React.useContext(UserDataContext);
+    const [openAlert, setOpenAlert] = React.useState(false);
+    const [alertMsg, setAlertMsg] = React.useState("");
 
     const logout = () => {
         let auth = getAuth();
         signOut(auth).then(() => {
             userContext.setUser(null)
-            alert("Sikeres kijelentkezés");
+            setAlertMsg("Sikeres kijelentkezés");
+            setOpenAlert(!openAlert);
         }).catch((error) => {
-          alert("nem sikerült kijelentkezni!");
+          setAlertMsg("nem sikerült kijelentkezni!");
+          setOpenAlert(!openAlert);
         });
+    }
+
+     const handleClose = () => {
+        setOpen(!open);
     }
 
     return (
         <div>
+            {openAlert && <Alert
+                content={<>
+                    <p>{alertMsg}</p>
+                </>}
+                handleClose={handleClose}
+            />}
             <Link to="#" className={classes.dropdownbtn} onClick={() => setOpen(open => !open)}>
                 Profile
             </Link>
