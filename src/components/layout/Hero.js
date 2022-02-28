5976a519a79e9790b9f7770383b6a0fb98fd0099
BHedi89 <hedi.besenyei89@gmail.com>
2022-02-28 14:33:54 +0100

    add hero title link back to main page

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,11 +1,12 @@
 import classes from "./Hero.module.css";
+import { Link } from "react-router-dom";
 
 const Hero = props => {
     return (
         <> 
             <section className={classes.hero} style={{backgroundImage: `url(${props.heroImage})`}}>
                 <div className={classes.herocontent}>
-                    <h1 className={classes.herotitle}>{props.title}</h1>
+                    <h1 className={classes.herotitle}><Link to="/">{props.title}</Link></h1>
                 </div>
             </section>
         </>
