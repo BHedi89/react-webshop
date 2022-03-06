f335a37a08a127d21627e5725e2dbd89f8c5d7fa
BHedi89 <hedi.besenyei89@gmail.com>
2022-03-06 14:27:42 +0100

    just logged in users can use starrating

--- src/components/layout/StarRating.js
+++ src/components/layout/StarRating.js
@@ -1,28 +1,51 @@
 import React from "react";
 import classes from "./StarRating.module.css";
+import { UserDataContext } from "../login/UserDataContext";
 
 const StarRating = (props) => {
     const [rating, setRating] = React.useState(props.ratingnum);
     const [hover, setHover] = React.useState(0);
-
+    let userContext = React.useContext(UserDataContext);
+    
     return (
-       <div className={classes.starrating}>
-           {[...Array(5)].map((star, index) => {
-                index += 1;
-                return (
-                    <button
-                        type="button"
-                        key={index}
-                        className={index <= (hover || rating) ? classes.on : classes.off}
-                        onClick={() => setRating(index)}
-                        onMouseEnter={() => setHover(index)}
-                        onMouseLeave={() => setHover(rating)}
-                    >
-                        <span className={classes.star}>&#9733;</span>
-                    </button>
-               )
-            })}
-       </div>
+        <>
+        {userContext?.user?.type === "user"
+            ?
+            <div className={classes.starrating}>
+                {[...Array(5)].map((star, index) => {
+                        index += 1;
+                        return (
+                            <button
+                                type="button"
+                                key={index}
+                                className={index <= (hover || rating) ? classes.on : classes.off}
+                                onClick={() => setRating(index)}
+                                onMouseEnter={() => setHover(index)}
+                                onMouseLeave={() => setHover(rating)}
+                            >
+                                <span className={classes.star}>&#9733;</span>
+                            </button>
+                    )
+                    })}
+            </div>
+            :
+            <div className={classes.starrating}>
+                {[...Array(5)].map((star, index) => {
+                        index += 1;
+                        return (
+                            <button
+                                type="button"
+                                key={index}
+                                className={index <= (hover || rating) ? classes.on : classes.off}
+                            >
+                                <span className={classes.star}>&#9733;</span>
+                            </button>
+                    )
+                    })}
+            </div>
+        }
+            
+       </>
     )
 }
 
