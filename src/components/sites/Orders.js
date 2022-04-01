e1c3187acd5745bca68b25db67ff381293d591ba
BHedi89 <hedi.besenyei89@gmail.com>
2022-04-01 17:49:46 +0200

    display separatedly active and passive orders

--- src/components/sites/Orders.js
+++ src/components/sites/Orders.js
@@ -3,8 +3,12 @@ import classes from "./Orders.module.css";
 import Navbar from "../layout/Navbar";
 import ShapeDivider from "../layout/ShapeDivider";
 import Footer from "../layout/Footer";
+import { UserDataContext } from "../login/UserDataContext";
 
 const Orders = () => {
+    let userContext = React.useContext(UserDataContext);
+    let orders = userContext.user.orders;
+
     return (
         <>
             <Navbar />
@@ -12,6 +16,68 @@ const Orders = () => {
             <ShapeDivider />
             <div className={classes.container}>
                 <h1>My orders</h1>
+                <div>
+                    <h2 className={classes.ordertitle}>Active orders</h2>  
+                    <table className={classes.table}>
+                        <tbody>
+                            <tr>
+                                <th>Image</th>
+                                <th>Product name</th>
+                                <th>Price</th>
+                            </tr>
+                            {orders.map(item => {
+                                return (
+                                    <>
+                                        {item.items.map(detail => {
+                                            return (
+                                                item.status === "active"
+                                                ?
+                                                <tr key={detail.id}>
+                                                    <td><img src={detail.productImage}/></td>
+                                                    <td>{detail.productName}</td>
+                                                    <td>${detail.productPrice}</td>
+                                                </tr>
+                                                : 
+                                                null
+                                            )
+                                        })}
+                                    </>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <h2 className={classes.ordertitle}>Passive orders</h2>  
+                    <table className={classes.table}>
+                        <tbody>
+                            <tr>
+                                <th>Image</th>
+                                <th>Product name</th>
+                                <th>Price</th>
+                            </tr>
+                            {orders.map(item => {
+                                return (
+                                    <>
+                                        {item.items.map(detail => {
+                                            return (
+                                                item.status === "passive"
+                                                ?
+                                                <tr key={detail.id}>
+                                                    <td><img src={detail.productImage}/></td>
+                                                    <td>{detail.productName}</td>
+                                                    <td>${detail.productPrice}</td>
+                                                </tr>
+                                                : 
+                                                null
+                                            )
+                                        })}
+                                    </>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
             <ShapeDivider />
             <div className={classes.footer}>
